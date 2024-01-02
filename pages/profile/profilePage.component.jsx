8601918a369a6ea932bb058f609ed53b329c67d8
Anthony <elmiranthony@gmail.com>
2024-01-02 19:45:42 +0100

    side menu page

---
+++ pages/profile/profilePage.component.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {FontAwesome5} from '@expo/vector-icons';
+import {
+	LogoutButton,
+	LogoutButtonText,
+	ProfileContainer,
+	ProfileInfoContainer,
+	UserProfileIcon,
+	ProfileInfoText,
+	ProfileSectionContainer,
+	ProfileTop,
+	SectionText,
+} from './profilePage.styles';
+
+const backgroundColor = '#111313';
+
+const UserProfilePage = ({navigation}) => {
+	const user = {
+		firstName: 'John',
+		lastName: 'Doe',
+		email: 'john.doe@example.com',
+		profileImage: '../../assets/default-user.jpg',
+	};
+	const sections = [
+		{icon: 'user', text: 'Profile'},
+		{icon: 'chalkboard-teacher', text: 'Become a teacher'},
+		{icon: 'cog', text: 'Settings'},
+	];
+
+	const handleSectionClick = (section) => {
+		alert(`Clicked on ${section.text}`);
+	};
+
+	const handleLogout = () => {
+		alert('Logout functionality goes here');
+	};
+
+	return (
+		<SafeAreaView style={{flex: 1, backgroundColor}}>
+			<ProfileContainer>
+				<ProfileTop>
+					<ProfileInfoContainer>
+						{/* <UserProfileIcon source={user.profileImage} /> */}
+						<ProfileInfoText>{`${user.firstName} ${user.lastName}`}</ProfileInfoText>
+						<ProfileInfoText>{user.email}</ProfileInfoText>
+					</ProfileInfoContainer>
+					{sections.map((section, index) => (
+						<ProfileSectionContainer
+							key={index}
+							onPress={() => handleSectionClick(section)}
+						>
+							<FontAwesome5 name={section.icon} size={24} color="#fcf9ff" />
+							<SectionText>{section.text}</SectionText>
+						</ProfileSectionContainer>
+					))}
+					<LogoutButton onPress={handleLogout}>
+						<LogoutButtonText>Logout</LogoutButtonText>
+					</LogoutButton>
+				</ProfileTop>
+			</ProfileContainer>
+		</SafeAreaView>
+	);
+};
+
+export default UserProfilePage;
